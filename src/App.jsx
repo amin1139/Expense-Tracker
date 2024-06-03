87c95eedf7346c414854fb12dd55582096eb75f4
Amin Ansari <aminansari135790@gmail.com>
2024-06-03 20:49:58 +0530

    ...

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
 
   const [inputValue, setInputValue] = useState('')
   const [inputPrice, setInputPrice] = useState('')
-  const [expenseData, setExpenseData] = useState(getLocal())
+  const [expenseData, setExpenseData] = useState([])
 
   useEffect(() => {
     localStorage.setItem('expenseList', JSON.stringify(expenseData))
@@ -42,15 +42,10 @@ function App() {
 
   const total = () =>{
     let sum = 0
-    if (expenseData === Array){
-      for(let i = 0; i < expenseData.length; i++){
-        sum = sum + Number(expenseData[i].Price)
-      }
-      return sum
+    for(let i = 0; i < expenseData.length; i++){
+      sum = sum + Number(expenseData[i].Price)
     }
-    else{
-      return
-    }
+    return sum
   }
 
   return (
@@ -74,8 +69,7 @@ function App() {
       </div>
 
       <div className="expenseList">
-        {expenseData === Array ? 
-          expenseData.map((v,i)=>{
+        {expenseData.map((v,i)=>{
           return(
             <div className="expenseLayout" key={i}>
               <div className='expense'>
@@ -85,7 +79,7 @@ function App() {
               <svg onClick={()=>{delIteam(i)}} xmlns="http://www.w3.org/2000/svg" height='1.3rem' viewBox="0 0 448 512"><path d="M135.2 17.7C140.6 6.8 151.7 0 163.8 0H284.2c12.1 0 23.2 6.8 28.6 17.7L320 32h96c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 96 0 81.7 0 64S14.3 32 32 32h96l7.2-14.3zM32 128H416V448c0 35.3-28.7 64-64 64H96c-35.3 0-64-28.7-64-64V128zm96 64c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16z"/></svg>
             </div>
           )
-        }) : ''}
+        })}
       </div>
 
 
